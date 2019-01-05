902d3d213f5e0c21f7c970309c24fbae86005f36
Jacob Stewart <jake@makedigitalgroup.com>
2019-01-05 12:17:12 -0600

    adding project

---
+++ src/Controller/SendMailController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jacobstewart
+ * Date: 1/4/19
+ * Time: 9:48 PM
+ */
+
+class SendMailController
+{
+
+}
---
+++ src/Form/ModuleConfigurationForm.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jacobstewart
+ * Date: 1/5/19
+ * Time: 11:22 AM
+ */
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ModuleConfigurationForm extends ConfigFormBase
+{
+
+    /**
+     * Gets the configuration names that will be editable.
+     *
+     * @return array
+     *   An array of configuration object names that are editable if called in
+     *   conjunction with the trait's config() method.
+     */
+    protected function getEditableConfigNames()
+    {
+        return [
+            'multisend.settings'
+        ];
+    }
+
+    /**
+     * Returns a unique string identifying the form.
+     *
+     * The returned ID should be a unique string that can be a valid PHP function
+     * name, since it's used in hook implementation names such as
+     * hook_form_FORM_ID_alter().
+     *
+     * @return string
+     *   The unique string identifying the form.
+     */
+    public function getFormId()
+    {
+        return 'multisend_admin_settings';
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        $config = $this->config('multisend.settings');
+        $query = \Drupal::entityQuery('node')
+            ->condition('status', 1)
+            ->condition('type', 'page');
+        $nids = $query->execute();
+        $nodes = \Drupal\node\Entity\Node::loadMultiple($nids);
+        $options = [];
+        foreach ($nodes as $node) {
+            $options[$node->id()] = $node->getTitle();
+        }
+
+        $form['smtp'] = [
+            '#type' => 'details',
+            '#title' => t('Pages'),
+            '#open' => true
+        ];
+
+        $form['smtp']['smtp_username'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('SMTP Username'),
+            '#default_value' => $config->get('smtp_username')
+        ];
+
+        $form['smtp']['smtp_password'] = [
+            '#type' => 'password',
+            '#title' => $this->t('SMTP Password'),
+            '#default_value' => $config->get('smtp_password')
+        ];
+
+        return parent::buildForm($form, $form_state);
+    }
+}
